3c125decb85dacb56288212166c5d9863c68bada
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-12-27 12:45:38 +0330

    algorithm changed

--- prj6/generic_tree.py
+++ prj6/generic_tree.py
@@ -1,15 +1,46 @@
-class Tree:
-    def __init__(self, data=10):
+class Node:
+    def __init__(self, data=None):
+        self.next = None
         self.data = data
-        self.children = []
-        self.root = None
+        self.previous = None
 
-    def insert(self, data):
-        if self:
-            self.children.append(Tree(data))
+
+class Tree(Node):
+    def __init__(self, data=10, **kwargs):
+        super().__init__(**kwargs)
+        self.data = data
+        self.children = Node()
+
+    def insert(self, data, root):
+        first_child = root.children
+        if root.children.data == None:
+            new_node = Tree(data)
+            root.children = new_node
+            root.children.previous = root
         else:
-            if self.root == None:
-                self.root = Tree(data)
+            current = first_child
+            while current.next != None:
+                current = current.next
+            new_node = Tree(data)
+            current.next = new_node
+            new_node.previous = current
 
-    def remove(self, data):
-        pass
+    # def remove(self, data, root):
+    #     while root.data != data:
+    #         for i in len(root.children):
+    #             self.remove(data, self.children[i])
+
+    #     return root.data
+
+
+sample = Tree(10)
+
+sample.insert(5, sample)
+sample.insert(7, sample)
+sample.insert(40, sample)
+sample.insert(60, sample)
+
+sample.insert(100, sample.children)
+sample.insert(200, sample.children)
+
+print(sample.children.)
