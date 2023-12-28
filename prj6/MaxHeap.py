d39c538b07dc07b318443a46357d69db1669ee20
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-12-28 17:12:21 +0330

    Maxheap DS completed

--- prj6/MaxHeap.py
+++ prj6/MaxHeap.py
@@ -1,47 +1,46 @@
 class MaxHeap:
     def __init__(self):
         self.heap_array = list()
-        self.order = 0
+        self.size = 0
 
     def insert(self, data):
         self.heap_array.append(data)
-        self.order += 1
-        self.heap_insert_order(len(self.heap_array) - 1)
+        self.size += 1
+        self.heap_insert_size(len(self.heap_array) - 1)
 
-    def heap_insert_order(self, order):
-        parent = int((order - 1) / 2)
+    def heap_insert_size(self, size):
+        parent = int((size - 1) / 2)
 
         if self.heap_array[parent] > 0:
-            if self.heap_array[order] > self.heap_array[parent]:
-                self.heap_array[order], self.heap_array[parent] = self.heap_array[parent], self.heap_array[order]
+            if self.heap_array[size] > self.heap_array[parent]:
+                self.heap_array[size], self.heap_array[parent] = self.heap_array[parent], self.heap_array[size]
 
-                self.heap_insert_order(parent)
+                self.heap_insert_size(parent)
 
-    def remove(self, data):
-        pass
+    def remove_root(self):
+        if self.size == 0:
+            print("Heap is Empty!!")
+        else:
+            max_value = self.heap_array[0]
 
-    def heap_remove_order(self, order, root_index):
-        left_child = (2 * order) + 1
-        right_child = (2 * order) + 2
+            self.heap_array[0] = self.heap_array[self.size - 1]
+            self.heap_array.pop()
+            self.size -= 1
 
-        root = root_index
+            self.remove_root_order(0)
 
-        pass
+    def remove_root_order(self, root_index):
+        largest = root_index
 
-    def display(self):
-        print(self.heap_array)
+        left_child = (2 * root_index) + 1
+        right_child = (2 * root_index) + 2
 
+        if left_child < self.size and self.heap_array[left_child] > self.heap_array[largest]:
+            largest = left_child
 
-sample = MaxHeap()
+        if right_child < self.size and self.heap_array[right_child] > self.heap_array[largest]:
+            largest = right_child
 
-
-sample.insert(40)
-sample.insert(30)
-sample.insert(24)
-sample.insert(10)
-sample.insert(20)
-sample.insert(5)
-sample.insert(3)
-
-
-sample.display()
+        if largest != root_index:
+            self.heap_array[root_index], self.heap_array[largest] = self.heap_array[largest], self.heap_array[root_index]
+            self.remove_root_order(largest)
