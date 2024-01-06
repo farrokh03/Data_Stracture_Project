862f177ffe29d9135cd85a1df917ad82792bd50c
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2024-01-06 11:26:09 +0330

    some problem fixed.

--- prj6/avl_tree.py
+++ prj6/avl_tree.py
@@ -7,12 +7,10 @@ class node(object):
 
 class avl(object): 
 
-	# Recursive function to insert key in 
-	# subtree rooted with node and returns 
-	# new root of subtree. 
+	
 	def insert(self, root, key): 
 	
-		# Step 1 - Perform normal BST 
+		
 		if not root: 
 			return node(key) 
 		elif key < root.val: 
@@ -20,30 +18,24 @@ class avl(object):
 		else: 
 			root.right = self.insert(root.right, key) 
 
-		# Step 2 - Update the height of the 
-		# ancestor node 
+		
 		root.height = 1 + max(self.getHeight(root.left), 
 						self.getHeight(root.right)) 
 
-		# Step 3 - Get the balance factor 
 		balance = self.getBalance(root) 
 
-		# Step 4 - If the node is unbalanced, 
-		# then try out the 4 cases 
-		# Case 1 - Left Left 
+	
 		if balance > 1 and key < root.left.val: 
 			return self.rightRotate(root) 
 
-		# Case 2 - Right Right 
+	
 		if balance < -1 and key > root.right.val: 
 			return self.leftRotate(root) 
 
-		# Case 3 - Left Right 
 		if balance > 1 and key > root.left.val: 
 			root.left = self.leftRotate(root.left) 
 			return self.rightRotate(root) 
 
-		# Case 4 - Right Left 
 		if balance < -1 and key < root.right.val: 
 			root.right = self.rightRotate(root.right) 
 			return self.leftRotate(root) 
@@ -55,17 +47,14 @@ class avl(object):
 		y = z.right 
 		T2 = y.left 
 
-		# Perform rotation 
 		y.left = z 
 		z.right = T2 
 
-		# Update heights 
 		z.height = 1 + max(self.getHeight(z.left), 
 						self.getHeight(z.right)) 
 		y.height = 1 + max(self.getHeight(y.left), 
 						self.getHeight(y.right)) 
 
-		# Return the new root 
 		return y 
 
 	def rightRotate(self, z): 
@@ -73,17 +62,14 @@ class avl(object):
 		y = z.left 
 		T3 = y.right 
 
-		# Perform rotation 
 		y.right = z 
 		z.left = T3 
 
-		# Update heights 
 		z.height = 1 + max(self.getHeight(z.left), 
 						self.getHeight(z.right)) 
 		y.height = 1 + max(self.getHeight(y.left), 
 						self.getHeight(y.right)) 
 
-		# Return the new root 
 		return y 
 	
 	def getHeight(self, root): 
